daa33c1d8ff4a343346ebd9e21030619f57af6ca
ChandanBose666 <bosechandan21gmail.com>
2022-09-26 03:11:23 +0530

    hashing of passwords and custom api errors class model implemented

--- controller/authController.js
+++ controller/authController.js
@@ -1,8 +1,19 @@
 import User from "../models/User.js";
 import {StatusCodes} from 'http-status-codes'
+import {BadRequestError} from "../errors/index.js";
 
 const register = async (req, res) => {
-    const user = await User.create(req.body);
+    const {name, email, password} = req.body;
+    if(!name || !email || !password){
+        throw new BadRequestError('Please provide all values');
+    }
+    const userAlreadyExists = await User.findOne({email});
+    // console.log("UserAlreadyExists" , userAlreadyExists);
+    if (userAlreadyExists){
+        throw new BadRequestError('User already exits. Please try with a different email')
+    }
+    const user = await User.create({name, email, password});
+    user.createJWT();
     res.status(StatusCodes.CREATED).json({ user });
 };
 const login = async (req, res) => res.send("User logged in");
