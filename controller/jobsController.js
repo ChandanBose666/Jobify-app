41ac5ba264d61dd6d153d68b6651bb80ea351b17
ChandanBose666 <bosechandan21gmail.com>
2022-10-11 02:36:40 +0530

    Refactored jobs controller for allJobs and edit job page

--- controller/jobsController.js
+++ controller/jobsController.js
@@ -1,12 +1,31 @@
+import Job from "../models/Job.js";
+import { StatusCodes } from "http-status-codes";
+import { BadRequestError, NotFoundError } from "../errors/index.js";
+
 const createJob = async (req, res) => {
-  await res.send("createJob");
+  const { company, position } = req.body;
+
+  if (!company || !position) {
+    throw new BadRequestError("Please provide all values!");
+  }
+
+  req.body.createdBy = req.user.userId;
+
+  const job = await Job.create(req.body);
+  res.status(StatusCodes.CREATED).json({ job });
 };
 
 const deleteJob = async (req, res) => {
   await res.send("Delete Job");
 };
 const getAllJobs = async (req, res) => {
-  await res.send("Get All Jobs");
+  const jobs = await Job.find({ createdBy: req.user.userId });
+
+  res.status(StatusCodes.OK).json({
+    jobs,
+    totalJobs: jobs.length,
+    numOfPages: 1,
+  });
 };
 const updateJob = async (req, res) => {
   await res.send("Update Job");
